ea8000050e1f83de1ed52cfd4fca78382804e155
qtdeveops <qtdeveops@gmail.com>
2021-03-20 23:26:26 -0400

    added 1.3 commit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 this is for developing only
 adding
 feature_1.0
-<<<<<<< HEAD
 sml change in master brnch
-=======
 feature2_1.0
->>>>>>> rel_1.0
+feature_rel1.3
--- test/main.py
+++ test/main.py
@@ -3,4 +3,4 @@ feature_1.0
 sml change in master brnch
 feature2_1.0
 rel_1.1
-
+feature_rel1.3
